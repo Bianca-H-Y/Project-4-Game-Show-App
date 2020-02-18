feaae670eedc75f585aad5a8c2449e788c86afcc
Bianca-why <56407953+Bianca-why@users.noreply.github.com>
2020-02-17 21:41:36 -0700

    code

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+  /**
+  * Create a Game class w/ a constructor that recieves 3 property & 6 methods:
+  * missed:
+  * track the number of missed guesses w/ initial value = 0
+  * phrases:
+  * and array of five phrase objects w/ only letters and spaces
+  * activePhrase:
+  * current phrase being guess; initial value = null within startGame() method, this property
+  * should be set to the phrase object retuned from a call in the getRandomPhrase() method.
+  * Methods
+  * startGame(): hides the start screen
+  * getRandomPhrase():
+  * handleInteraction():
+  * removeLife():
+  * checkForWin():
+  * gameOver():
+  */
+ 
+ class phrase {
+    constructor (phrase) {
+        this.phrase = phrase
+    }
+}
--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ /**
+  * Create a Phrase class w/ a constructor that recieves 1 property & 4 methods:
+  * phrase property :
+  * and intializes a phrase-this will be the actually phrase the user is guessing.
+  * the property should be converted to all lower case.
+  * Methods
+  * addpPhraseToDisplay():
+  * display letter placeholders when the game starts
+  * when the corrct letter is guess showMatchLetter replaces the empty box
+  * makes sure the phrase uses the CSS for letters and the CSS for spaces
+  * checkLetter():
+  * check to see if player selections matches any letters in the phrase
+  * showMatchedLetter():
+  * revels letters that match player selection
+  * to reveal select all letter DOM elements w/ CSS class name that match letter
+  * then replace e/ match with hide CSS class with Show CSS class
+  */
+
+  class phrase {
+      constructor (phrase) {
+          this.phrase = phrase
+      }
+  }
