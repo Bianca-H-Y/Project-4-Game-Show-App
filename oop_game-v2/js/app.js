ca77166ee4d3d6bc3b46a2bb9f14ce211d763dbb
Bianca-why <56407953+Bianca-why@users.noreply.github.com>
2020-02-22 23:21:28 -0700

    Step 8 done

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -16,7 +16,21 @@ const game = new Game();
 game.getRandomPhrase().addPhraseToDisplay();
 note:step 6 pass
 */
+/** 
 const game = new Game();
 game.startGame();
 console.log('Active Phrase - phrase: ${game.activePhrase.phrase}');
 //Success!!--actually log not showing quote
+*/
+
+
+//create game variable set to nothing
+let game = null;
+//Create a click event listener for the start game button: <button id="btn__reset">Start Game</button>
+const startButton = document.getElementById('btn__reset');
+startButton.addEventListener('click', e => {
+    //create a new Game object and use the startgame method();
+    game = new Game();
+    game.startGame();
+});
+
